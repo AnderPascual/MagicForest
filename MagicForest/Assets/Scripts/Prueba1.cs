c85f8bd890ae7bb9b1b02cdc6bf271b8b43ad9b3
danisolde4 <154700372+danisolde4@users.noreply.github.com>
2023-12-23 20:55:18 +0100

    prueba Dani

--- MagicForest/Assets/Scripts/Prueba1.cs
+++ MagicForest/Assets/Scripts/Prueba1.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Prueba1 : MonoBehaviour
 {
+    [SerializeField] int PruebaDani;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
